28594afcc84ea4c74211a67059ce05cb36d0f64c
gkpeel <gkpeel@gmail.com>
2024-03-11 17:57:55 -0400

    US123 - added Class

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const userName = 'Geoffrey'
-console.log(userName)
+const userName = "Geoffrey";
+console.log(userName);
+
+class User {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  printName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const user = new User("Geoff", "Peel");
