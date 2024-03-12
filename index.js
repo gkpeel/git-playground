ce4197461433a470437cad48976e262a66a89a96
Geoff P <gkpeel@gmail.com>
2024-03-11 20:42:31 -0400

    Feature2  dev (#2)
    * US123 - added Class
    
    * US123 - printName

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const userName = "Geoffrey Peel";
+const userName = "Geoffrey";
 console.log(userName);
+
+class User {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  printName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const user = new User("Geoff", "Peel");
+user.printName();
